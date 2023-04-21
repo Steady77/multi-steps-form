c6b849814b0cf6c2cb6b505b03ff4b336830f653
Surov Denis <90111915+Steady77@users.noreply.github.com>
2023-04-21 10:52:14 +0300

    chore: update routes data

--- src/routes/routes.data.ts
+++ src/routes/routes.data.ts
@@ -1,5 +1,9 @@
 import { FC } from 'react';
 import CommonStep from '../components/screens/common-step/common-step';
+import OwnershipFormStep from '../components/screens/ownership-form-step/ownership-form-step';
+import RegistrAddressStep from '../components/screens/registr-address-step/registr-address-step';
+import ResidentialAddressStep from '../components/screens/residential-address-step/residential-address-step';
+import SocialStep from '../components/screens/social-step/social-step';
 
 interface Routes {
 	path: string;
@@ -12,19 +16,19 @@ export const routes: Routes[] = [
 		component: CommonStep,
 	},
 	{
-		path: '/',
-		component: CommonStep,
+		path: '/ownership-form',
+		component: OwnershipFormStep,
 	},
 	{
-		path: '/',
-		component: CommonStep,
+		path: '/register-address',
+		component: RegistrAddressStep,
 	},
 	{
-		path: '/',
-		component: CommonStep,
+		path: '/residential-address',
+		component: ResidentialAddressStep,
 	},
 	{
-		path: '/',
-		component: CommonStep,
+		path: '/social-form',
+		component: SocialStep,
 	},
 ];
