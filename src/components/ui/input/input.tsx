758b3dc6c7a8e726cc51f09231bd0f11bafe73c0
Surov Denis <90111915+Steady77@users.noreply.github.com>
2023-04-27 19:25:30 +0300

    feat: add custom input

---
+++ src/components/ui/input/input.tsx
@@ -0,0 +1,36 @@
+import { forwardRef, InputHTMLAttributes } from 'react';
+import styles from './input.module.scss';
+import cn from 'classnames';
+
+interface InputType extends InputHTMLAttributes<HTMLInputElement> {
+	error?: string;
+	label?: string;
+}
+
+const Input = forwardRef<HTMLInputElement, InputType>(
+	(
+		{ placeholder, error, type = 'text', className, label, children, ...rest },
+		ref,
+	) => {
+		return (
+			<div className={cn(styles.inputWrapper, className)}>
+				<p className={styles.label}>{label}</p>
+				<label>
+					<input
+						placeholder={placeholder}
+						ref={ref}
+						type={type}
+						{...rest}
+						className={styles.input}
+					/>
+					<span>{children}</span>
+				</label>
+				{error && <div className={styles.error}>{error}</div>}
+			</div>
+		);
+	},
+);
+
+Input.displayName = 'Input';
+
+export default Input;
