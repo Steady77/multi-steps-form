b8739ddf4d5f8c6f765ba0fbd5013596ddc603f6
Surov Denis <90111915+Steady77@users.noreply.github.com>
2023-05-04 09:43:50 +0300

    ui: add checkbox component

---
+++ src/components/ui/checkbox/checkbox.tsx
@@ -0,0 +1,20 @@
+import { InputHTMLAttributes, forwardRef } from 'react';
+import styles from './checkbox.module.scss';
+
+type CheckboxType = InputHTMLAttributes<HTMLInputElement>;
+
+const Checkbox = forwardRef<HTMLInputElement, CheckboxType>(
+	({ children, ...rest }, ref) => {
+		return (
+			<label className={styles.label}>
+				<input className={styles.input} type="checkbox" ref={ref} {...rest} />
+				<span className={styles.span}></span>
+				<span>{children}</span>
+			</label>
+		);
+	},
+);
+
+Checkbox.displayName = 'Checkbox';
+
+export default Checkbox;
