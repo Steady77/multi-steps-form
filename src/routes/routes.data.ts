6c4841e18bfcf1d843c684cec282697faff52b24
Surov Denis <90111915+Steady77@users.noreply.github.com>
2023-05-05 12:29:16 +0300

    chore: fix routes

--- src/routes/routes.data.ts
+++ src/routes/routes.data.ts
@@ -2,8 +2,6 @@ import { FC } from 'react';
 import CommonStep from '../components/screens/common-step/common-step';
 import OwnershipFormStep from '../components/screens/ownership-form-step/ownership-form-step';
 import RegistrAddressStep from '../components/screens/registr-address-step/registr-address-step';
-import ResidentialAddressStep from '../components/screens/residential-address-step/residential-address-step';
-import SocialStep from '../components/screens/social-step/social-step';
 
 interface Routes {
 	path: string;
@@ -22,13 +20,5 @@ export const routes: Routes[] = [
 	{
 		path: '/register-address',
 		component: RegistrAddressStep,
-	},
-	{
-		path: '/residential-address',
-		component: ResidentialAddressStep,
-	},
-	{
-		path: '/social-form',
-		component: SocialStep,
 	},
 ];
