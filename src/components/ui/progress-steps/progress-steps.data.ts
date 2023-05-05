1deb82717aa4cfb39f1ec79741b9cb8b3c9d6d00
Surov Denis <90111915+Steady77@users.noreply.github.com>
2023-05-05 12:03:58 +0300

    fix: add path for display corrent form step

--- src/components/ui/progress-steps/progress-steps.data.ts
+++ src/components/ui/progress-steps/progress-steps.data.ts
@@ -1,27 +1,33 @@
 interface Steps {
 	label: string;
 	step: number;
+	path: string;
 }
 
 export const steps: Steps[] = [
 	{
 		label: 'Общие',
 		step: 1,
+		path: '/',
 	},
 	{
 		label: 'Форма собственности',
 		step: 2,
+		path: '/ownership-form',
 	},
 	{
 		label: 'Адрес регистрации',
 		step: 3,
+		path: '/register-address',
 	},
 	{
 		label: 'Адрес проживания',
 		step: 4,
+		path: '/residential-address',
 	},
 	{
 		label: 'Социальные сети',
 		step: 5,
+		path: '/social-form',
 	},
 ];
--- src/components/ui/progress-steps/progress-steps.tsx
+++ src/components/ui/progress-steps/progress-steps.tsx
@@ -2,12 +2,26 @@ import { FC } from 'react';
 import { steps } from './progress-steps.data';
 import styles from './progress-steps.module.scss';
 import cn from 'classnames';
+import { useFormsState } from '../../../hooks/use-forms-state';
+import { useLocation } from 'react-router-dom';
 
 const ProgressSteps: FC = () => {
+	const { pathname } = useLocation();
+
+	const {
+		formsState: { activeStep },
+	} = useFormsState();
+
 	return (
 		<ul>
-			{steps.map(({ label, step }) => (
-				<li key={step} className={cn(styles.item)}>
+			{steps.map(({ label, step, path }) => (
+				<li
+					key={step}
+					className={cn(styles.item, {
+						[styles.active]: path === pathname,
+						[styles.checked]: step < activeStep,
+					})}
+				>
 					<span className={styles.circle}>{step}</span>
 					<span className={styles.text}>{label}</span>
 				</li>
